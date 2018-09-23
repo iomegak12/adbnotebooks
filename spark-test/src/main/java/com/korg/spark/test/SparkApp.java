acaed29d593a8b74023515bdeea60ff871947bd8
iomegak12 <jd.ramkumar@gmail.com>
2018-09-23 15:42:56 +0530

    simple

---
+++ spark-test/src/main/java/com/korg/spark/test/SparkApp.java
@@ -0,0 +1,11 @@
+package com.korg.spark.test;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.SparkSession;
+public class SparkApp {
+public static void main(String[] args) {
+SparkSession spark = SparkSession.builder().appName("SparkApp").config("spark.master", "local").getOrCreate();
+Dataset<String> textFile = spark.read().textFile(args[0]);
+System.out.println("Number of lines " + textFile.count());
+spark.stop();
+}
+}
